d6d48e013c5133e16e9c562077a14cf7f9088f51
Vaughn2400 <kennethgolosinda24@gmail.com>
2022-03-23 18:27:43 +0800

    Update App.js

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import React from "react";
 import { StyleSheet, Text, SafeAreaView } from "react-native";
 import SignInScreen from "./src/screens/SignInScreen";
 
+
 export default function App() {
   return (
     <SafeAreaView style={styles.root}>
-      <SignInScreen />
+      <SignInScreen /> 
     </SafeAreaView>
   );
 }
