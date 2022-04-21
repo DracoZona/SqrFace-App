bbc53d5824c431e3ae53eab151fbe85267f691e1
Vaughn2400 <kennethgolosinda24@gmail.com>
2022-04-21 21:16:55 +0800

    Update FaceDetectScreen.js

--- src/screens/FaceDetectScreen/FaceDetectScreen.js
+++ src/screens/FaceDetectScreen/FaceDetectScreen.js
@@ -9,17 +9,6 @@ import {
   Button,
 } from "react-native";
 import React, { useState, useEffect } from "react";
-import Logo from "../../../assets/images/sqrlogo.png";
-import CustomInput from "../../components/CustomInput";
-import CustomButton from "../../components/CustomButton";
-import CustomNumberInput from "../../components/CustomNumberInput/CustomNumberInput";
-import Navigation from "../../navigation";
-import { useNavigation } from "@react-navigation/native";
-import { styleProps } from "react-native-web/dist/cjs/modules/forwardedProps";
-import SignInScreen from "../SignInScreen";
-import DropDownPicker from "react-native-dropdown-picker";
-import Constants from "../../utils/constants";
-import { ProgressSteps, ProgressStep } from "react-native-progress-steps";
 import { Camera } from 'expo-camera';
 import * as Permissions from "expo-permissions";
 
@@ -28,9 +17,11 @@ const FaceDetectScreen = () => {
   //Camera Instance
 
   const [hasCameraPermission, sethasCameraPermission] = useState(null);
+  const [galleryPermission, setGalleryPermission] = useState(null);
   const [camera, setCamera] = useState(null);
   const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const [imageUri, setImageUri] = useState(null);
 
 
   useEffect(() => {
@@ -38,15 +29,29 @@ const FaceDetectScreen = () => {
       // const cameraStatus = await Camera.requestMicrophonePermissionsAsync();
       const { cameraStatus } = await Permissions.askAsync(Permissions.CAMERA);
       sethasCameraPermission(cameraStatus.status === 'granted');
+      const imagePermission = await ImagePicker.getMediaLibraryPermissionsAsync();
+      console.log(imagePermission.status);
+  
+      setGalleryPermission(imagePermission.status === 'granted');
+  
+      if (
+        imagePermission.status !== 'granted' &&
+        cameraPermission.status !== 'granted'
+      ) {
+        alert('Permission for media access needed.');
+      }
+
+      
     })();
   }, []);
 
   const takePicture = async () => {
     if (camera) {
-      const data = await camera.takePicture(null)
-      setImage(data.uri);
+      const data = await camera.takePictureAsync(null);
+      console.log(data.uri);
+      setImageUri(data.uri);
     }
-  }
+  };
 
   if (hasCameraPermission === false) {
     return <Text>No Camera Access</Text>;
@@ -73,9 +78,9 @@ const FaceDetectScreen = () => {
               setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back);
             }}
           />
-          <Button title={'Take Picture'} />
+      <Button title={'Take Picture'} onPress={takePicture} />
+      {imageUri && <Image source={{ uri: imageUri }} style={{ flex: 1 }} />}
 
-          {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
         </View>
       </View>
     </ScrollView>
