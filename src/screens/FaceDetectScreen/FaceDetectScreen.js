44fc6097a2490382097f69c5ac200e6430ff372d
Vaughn2400 <kennethgolosinda24@gmail.com>
2022-04-23 19:00:27 +0800

    facedetect

--- src/screens/FaceDetectScreen/FaceDetectScreen.js
+++ src/screens/FaceDetectScreen/FaceDetectScreen.js
@@ -9,11 +9,12 @@ import {
   Button,
 } from "react-native";
 import React, { useState, useEffect } from "react";
-import { Camera } from 'expo-camera';
+import { Camera } from "expo-camera";
 import * as Permissions from "expo-permissions";
+// import ReactNativeBlobUtil from 'react-native-blob-util'
+
 
 const FaceDetectScreen = () => {
-
   //Camera Instance
 
   const [hasCameraPermission, sethasCameraPermission] = useState(null);
@@ -23,33 +24,59 @@ const FaceDetectScreen = () => {
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [imageUri, setImageUri] = useState(null);
 
-
   useEffect(() => {
     (async () => {
       // const cameraStatus = await Camera.requestMicrophonePermissionsAsync();
       const { cameraStatus } = await Permissions.askAsync(Permissions.CAMERA);
-      sethasCameraPermission(cameraStatus.status === 'granted');
-      const imagePermission = await ImagePicker.getMediaLibraryPermissionsAsync();
+      sethasCameraPermission(cameraStatus.status === "granted");
+      const imagePermission =
+        await ImagePicker.getMediaLibraryPermissionsAsync();
       console.log(imagePermission.status);
-  
-      setGalleryPermission(imagePermission.status === 'granted');
-  
-      if (
-        imagePermission.status !== 'granted' &&
-        cameraPermission.status !== 'granted'
-      ) {
-        alert('Permission for media access needed.');
-      }
 
-      
+      setGalleryPermission(imagePermission.status === "granted");
+
+      if (
+        imagePermission.status !== "granted" &&
+        cameraPermission.status !== "granted"
+      ) {
+        alert("Permission for media access needed.");
+      }
     })();
   }, []);
+
+
+  const ReactNativeBlobUtil = require('react-native-blob-util').default
+  
+  sendImg = () => {
+    ReactNativeBlobUtil.fetch(
+      "POST",
+      "http://127.0.0.1:5000/upload_file",
+      {
+        sendimage: JSON.stringify({
+          path: expoSnapped,
+          mode: "add",
+          autorename: true,
+          mute: false,
+        }),
+        "Content-Type": "application/json",
+      },
+      base64ImageString
+    )
+      .then((res) => {
+        console.log(res.text());
+      })
+      .catch((err) => {
+        // error handling ..
+      });
+  };
 
   const takePicture = async () => {
     if (camera) {
       const data = await camera.takePictureAsync(null);
       console.log(data.uri);
       setImageUri(data.uri);
+      expoSnapped = data.uri;
+      sendImg();
     }
   };
 
@@ -58,29 +85,31 @@ const FaceDetectScreen = () => {
   }
   //End of Camera Instance
 
-
-
   // User Interface
   return (
     <ScrollView>
       <View style={styles.root}>
         <View style={styles.cameraContainer}>
-          <Camera ref={ref => setCamera(ref)}
+          <Camera
+            ref={(ref) => setCamera(ref)}
             style={styles.fixedRatio}
             type={type}
-            ratio={'16:3'}
+            ratio={"16:3"}
           />
 
           <Button
             style={styles.buttonStart}
             title="Flip Camera"
             onPress={() => {
-              setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back);
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
             }}
           />
-      <Button title={'Take Picture'} onPress={takePicture} />
-      {imageUri && <Image source={{ uri: imageUri }} style={{ flex: 1 }} />}
-
+          <Button title={"Take Picture"} onPress={takePicture} />
+          {imageUri && <Image source={{ uri: imageUri }} style={{ flex: 1 }} />}
         </View>
       </View>
     </ScrollView>
@@ -97,14 +126,14 @@ const styles = StyleSheet.create({
   cameraContainer: {
     width: "100%",
     flex: 1,
-    flexDirection: 'column',
-    marginTop: 150
+    flexDirection: "column",
+    marginTop: 150,
   },
 
   fixedRatio: {
     flex: 1,
-    aspectRatio: 1
-  }
+    aspectRatio: 1,
+  },
 });
 
 export default FaceDetectScreen;
