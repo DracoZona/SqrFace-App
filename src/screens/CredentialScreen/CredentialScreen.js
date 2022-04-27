25791daaab9d28c403328e74f5597b2b743ed997
Vaughn2400 <kennethgolosinda24@gmail.com>
2022-04-27 22:19:19 +0800

    Update CredentialScreen.js

--- src/screens/CredentialScreen/CredentialScreen.js
+++ src/screens/CredentialScreen/CredentialScreen.js
@@ -17,8 +17,8 @@ const CredentialScreen = () => {
       if(user){
         setUser(user)
         console.log(user)
-        if(user.Image) {
-          refs.storage.child(user.Image).getDownloadURL().then(v => setImage(v)).catch(console.error)
+        if(user.QrImage) {
+          refs.storage.child(user.QrImage).getDownloadURL().then(v => setImage(v)).catch(console.error)
         }
       }
     });
@@ -30,28 +30,29 @@ const CredentialScreen = () => {
     <ScrollView>
       <View style={styles.root}>
         <View>
-          <Image style={{width: 100, height: 100}} source={{uri: image}} />
+          
           <Text>User Credential</Text>
-          <Text>Name:{user["Name"]}</Text>
-          <Text>Gender:</Text>
-          <Text>Birthdate:</Text>
-          <Text>Primary Mobile No.:</Text>
-          <Text>Email:</Text>
+          <Text>Name: {user["Name"]}</Text>
+          <Text>Gender: {user["Gender"]}</Text>
+          <Text>Birthdate: {user["Birthdate"]}</Text>
+          <Text>Primary Mobile No.: {user["Mobile"]}</Text>
+          <Text>Email: {user["Email"]}</Text>
           
 
         </View>
 
         <View>
           <Text>Vaccination Status</Text>
-          <Text>Vaccine Name:</Text>
-          <Text>Booster Name:</Text>
-          <Text>Date of 1st Dose:</Text>
-          <Text>Date of 2nd Dose:</Text>
-          <Text>Date of Booster Shot:</Text>
+          <Text>Vaccine Name: {user["Vaccine Name"]}</Text>
+          <Text>Booster Name: {user["Booster Name"]}</Text>
+          <Text>Date of 1st Dose: {user["Date of 1st Dose"]}</Text>
+          <Text>Date of 2nd Dose: {user["Date of 2nd Dose"]}</Text>
+          <Text>Date of Booster Shot: {user["Date of Booster Shot"]}</Text>
         </View>
 
         <View>
           <Text>User's Qr Code</Text>
+          <Image style={{width: 300, height: 500}} source={{uri: image}} />
         </View>
       </View>
     </ScrollView>
